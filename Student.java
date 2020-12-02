5a882c8a21f8fa904be1e73764cf58548c203136
BaC <cba.chenbingan@gmail.com>
2020-12-02 20:59:57 +0800

    Two proj for ppt4Lab1

---
+++ Student.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Student {
+
+	String name;
+	double grade1, grade2, avg;
+	Scanner scan = new Scanner(System.in);
+
+	public Student(String studentName)
+	{
+		name = studentName;
+	}
+
+	public void inputGrades()
+	{
+		System.out.println("Input your score of the first test: ");
+		grade1 = scan.nextDouble();
+		System.out.println("Input your score of the second test: ");
+		grade2 = scan.nextDouble();
+	}
+
+	public double getAverage()
+	{
+		avg = (grade1 + grade2) / 2;
+		return avg;
+	}
+
+	public String toString()
+	{
+		return "Name: " + name + "\t" + "Test1: " + grade1 + "\t" + "Test2: " + grade2 + "\t Avg: " + avg;
+	}
+
+}
