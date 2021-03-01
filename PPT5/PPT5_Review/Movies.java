0285ed51cb69523a34858b27dc7d0ecc60017593
BaC <cba.chenbingan@gmail.com>
2021-03-01 19:55:21 +0800

    create Movies.java

---
+++ PPT5/PPT5_Review/Movies.java
@@ -0,0 +1,10 @@
+public class Movies{
+    static class DVD{
+        private String title;
+        private int year;
+        private double prize;
+        private boolean bluray;
+
+        public DVD()
+    }
+}
